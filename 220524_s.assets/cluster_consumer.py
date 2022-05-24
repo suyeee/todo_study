295df245148376b03bfec4cadc0e7e252456add1
suyeee <suyeon0727@naver.com>
2022-05-24 23:32:39 +0900

    0524-file error

--- 220524_s.assets/cluster_consumer.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aaad253876ad29690038add3e00a36effd1cb1e48818531c09a259a5f3f6601b
-size 554
--- 220524_s.assets/producer.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3d18ccd7fe58fece852039d95c28b6a1e421c46fa903c202a2d957c412171f9e
-size 553
